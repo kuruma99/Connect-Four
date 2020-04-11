e17757a283c8c6bcaf5d89cf3480f9d96c720dd5
Mridul Singh <msmridulsingh@gmail.com>
2020-04-11 23:05:16 +0530

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -1,36 +1,43 @@
-import numpy as np
-import pygame
+import numpy as np # for easy play with arrays
+import pygame 
 import sys
 import math
-import pygame.freetype
-import pygame.font
+# import pygame.freetype
+import pygame.font # for font object and rendering data on screen
 
+# Specifying colors via rgb
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
 
+# Default row and column numbers of the game
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
+# Function to create the board
 def create_board():
 	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
 	return board
 
+# Function to drop pieces in the place
 def drop_piece(board, row, col, piece):
 	board[row][col] = piece
 
+# Function to check if the position is valid or not
 def is_valid_location(board, col):
 	return board[ROW_COUNT-1][col] == 0
 
+# Function to get the empty of row of specified column
 def get_next_open_row(board, col):
 	for r in range(ROW_COUNT):
 		if board[r][col] == 0:
 			return r
-
+# Function to print the board on console
 def print_board(board):
 	print(np.flip(board, 0))
 
+# Function to check if the winning move has been played
 def winning_move(board, piece):
 	# Check horizontal locations for win
 	for c in range(COLUMN_COUNT-3):
@@ -54,8 +61,8 @@ def winning_move(board, piece):
 	for c in range(COLUMN_COUNT-3):
 		for r in range(3, ROW_COUNT):
 			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-				return True
-
+				return urn i
+# Function to draw board via pygame
 def draw_board(board):
 	for c in range(COLUMN_COUNT):
 		for r in range(ROW_COUNT):
@@ -70,30 +77,29 @@ def draw_board(board):
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
 
-
-board = create_board()
-print_board(board)
+board = create_board() # Create board 
+print_board(board) # Print the board
 game_over = False
-turn = 0
+turn = 0 # to decide whose turn is it
 
-pygame.init()
+pygame.init() # initiate pygame
 
-SQUARESIZE = 100
+SQUARESIZE = 100 
 
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT+1) * SQUARESIZE
+width = COLUMN_COUNT * SQUARESIZE # width of game window
+height = (ROW_COUNT+1) * SQUARESIZE # height of game window
 
-size = (width, height)
+size = (width, height) # size of game window
 
-RADIUS = int(SQUARESIZE/2 - 5)
+RADIUS = int(SQUARESIZE/2 - 5) # radius of circle
 
-screen = pygame.display.set_mode(size)
-draw_board(board)
+screen = pygame.display.set_mode(size) # setting screen for game
+draw_board(board) # drawing the board using pygame
 pygame.display.update()
 
-myfont = pygame.font.SysFont("monospace", 75)
+myfont = pygame.font.SysFont("monospace", 75) # create a font object
 
-while not game_over:
+while not game_over: # Main game loop
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -140,10 +146,10 @@ while not game_over:
 						screen.blit(label, (40,10))
 						game_over = True
 
-			print_board(board)
-			draw_board(board)
+			print_board(board) # print board in shell
+			draw_board(board) # draw board in game window
 
-			turn += 1
+			turn += 1 # to change the turns
 			turn = turn % 2
 
 			if game_over:
